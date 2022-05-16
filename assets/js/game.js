0b55e113f91a11d9f0c25dd8dbd799e02216aeb3
lillielove <lillielove@gmail.com>
2022-05-16 12:25:44 -0700

    add js console log tests

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alertt");
+// function called "fight"
+
+var playerName= window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log(10+10);
+console.log("our name is " + playerName);
+
+function fight(){
+    window.alert("fight has begun");
+}
+
+// fight();
